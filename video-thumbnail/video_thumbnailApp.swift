5d1f6dc4f9ce32153b2e9d0d94f4b77ffdc10ad4
nallapati <sandeep.nallapati@onsite.co.in>
2025-11-07 17:28:55 +0530

    Initial Commit

---
+++ video-thumbnail/video_thumbnailApp.swift
@@ -0,0 +1,17 @@
+//
+//  video_thumbnailApp.swift
+//  video-thumbnail
+//
+//  Created by Sandeep on 07/11/25.
+//
+
+import SwiftUI
+
+@main
+struct video_thumbnailApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
